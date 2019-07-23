e34987b2e2666430c33b477fe33bbdf2de43f849
Vitaliy Larin <vitaly_news@mail.ru>
2019-07-23 20:57:13 +0300

    fix windows path

--- www/index.php
+++ www/index.php
@@ -2,7 +2,7 @@
 
 try {
     spl_autoload_register(function (string $className) {
-        require_once __DIR__ . '/../src/' . $className . '.php';
+        require_once __DIR__ . '/../src/' . str_replace("\\","/",$className) . '.php';
     });
 
     $route = $_GET['route'] ?? '';
